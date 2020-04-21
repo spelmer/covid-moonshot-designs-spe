6dd7d1d9e6aff9afb6d2c5d9444b9fe2eca77142
Sidney Elmer <7726838+sidneypaulymer@users.noreply.github.com>
2020-04-21 13:14:38 -0700

    cleanup qmmm files, including replacing old files with new files for SPE_0028, SPE_0035, SPE_0040, and SPE_0047

--- Round1/01_x0434_x0678_modifications/02_extend_x0434_or_x0678_redo/02_qmmm/RUN-cpptraj-dyn.zsh
+++ Round1/01_x0434_x0678_modifications/02_extend_x0434_or_x0678_redo/02_qmmm/RUN-cpptraj-dyn.zsh
@@ -1,7 +1,6 @@
 #!/bin/zsh
 
-#for mol in 00{24..47}; do
-for mol in 0026 0027; do
+for mol in 00{24..47}; do
     echo "=== SPE_${mol} ==="
     cpptraj -p comp-SPE_${mol}.prmtop <<- CPPTRAJ
         trajin mdcomp-SPE_${mol}.mdcrd 1 last
