18332dd2a5e5b15a6c1e9fa7e0245a9fae4bb8b4
Sidney Elmer <7726838+sidneypaulymer@users.noreply.github.com>
2020-04-21 13:45:36 -0700

    combine directories

--- Round1/01_x0434_x0678_modifications/01_extend_x0434_or_x0678/RUN-md.zsh
+++
@@ -1,5 +0,0 @@
-#!/bin/zsh
-
-echo "=== SPE_0021 ==="
-sander -O -i min.mdin -o mincomp-SPE_0021H.mdout -p comp-SPE_0021H.prmtop -c comp-SPE_0021H.inpcrd -r mincomp-SPE_0021H.restrt
-#sander -O -i md.mdin -o mdcomp-SPE_0021H.mdout -p comp-SPE_0021H.prmtop -c mincomp-SPE_0021H.restrt -r mdcomp-SPE_0021H.restrt -x mdcomp-SPE_0021H.mdcrd
